906140a24b52b634a2fc54ad2120d181430288f1
fatchurrahman.fatchurrahman <fatchurrahman.fatchurrahman@adira.co.id>
2020-07-30 11:53:37 +0700

    test format

--- src/main/java/co/id/adira/moservice/contentservice/repository/PromoRepository.java
+++ src/main/java/co/id/adira/moservice/contentservice/repository/PromoRepository.java
@@ -12,12 +12,13 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PromoRepository extends JpaRepository<Promo, Long> {
-  @Query(value = "SELECT * FROM m_promo p WHERE p.zone_id = :zoneId AND p.is_active = true AND p.is_deleted = false AND p.available_until > :currentDate AND p.available_from < :currentDate ORDER BY :#{#pageable}", nativeQuery = true)
-  List<Promo> findAllByZoneIdAndMore(@Param("zoneId") Long zoneId, @Param("currentDate") Date currentDate, @Param("pageable") Pageable pageable);
+	@Query(value = "SELECT * FROM m_promo p WHERE p.zone_id = :zoneId AND p.is_active = true AND p.is_deleted = false AND p.available_until > :currentDate AND p.available_from < :currentDate ORDER BY :#{#pageable}", nativeQuery = true)
+	List<Promo> findAllByZoneIdAndMore(@Param("zoneId") Long zoneId, @Param("currentDate") Date currentDate,
+			@Param("pageable") Pageable pageable);
 
-  @Query(value = "SELECT * FROM m_promo p WHERE p.is_active = true AND p.is_deleted = false AND p.available_until > :currentDate AND p.available_from < :currentDate ORDER BY :#{#pageable}", nativeQuery = true)
-  List<Promo> findAllAndMore(@Param("currentDate") Date currentDate, @Param("pageable") Pageable pageable);
+	@Query(value = "SELECT * FROM m_promo p WHERE p.is_active = true AND p.is_deleted = false AND p.available_until > :currentDate AND p.available_from < :currentDate ORDER BY :#{#pageable}", nativeQuery = true)
+	List<Promo> findAllAndMore(@Param("currentDate") Date currentDate, @Param("pageable") Pageable pageable);
 
-  @Query(value = "SELECT * FROM m_promo p WHERE p.id = :id AND p.is_active = true AND p.is_deleted = false AND p.available_until > :currentDate AND p.available_from < :currentDate ", nativeQuery = true)
-  Optional<Promo> findByIdAndMore(@Param("id") Long id, @Param("currentDate") Date currentDate);
+	@Query(value = "SELECT * FROM m_promo p WHERE p.id = :id AND p.is_active = true AND p.is_deleted = false AND p.available_until > :currentDate AND p.available_from < :currentDate ", nativeQuery = true)
+	Optional<Promo> findByIdAndMore(@Param("id") Long id, @Param("currentDate") Date currentDate);
 }
