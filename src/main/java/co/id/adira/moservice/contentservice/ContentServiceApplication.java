f3ee8123d3b70da13549ab886977b396f74c6d89
fatchurrahman.fatchurrahman <fatchurrahman.fatchurrahman@adira.co.id>
2020-08-10 11:26:49 +0700

    enable cache redis

--- src/main/java/co/id/adira/moservice/contentservice/ContentServiceApplication.java
+++ src/main/java/co/id/adira/moservice/contentservice/ContentServiceApplication.java
@@ -3,9 +3,11 @@ package co.id.adira.moservice.contentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableCaching
 @ComponentScan
 @EntityScan({"co.id.adira.moservice.model",
 "co.id.adira.moservice.contentservice.model"})
--- src/main/java/co/id/adira/moservice/contentservice/repository/PilihanLainRepository.java
+++ src/main/java/co/id/adira/moservice/contentservice/repository/PilihanLainRepository.java
@@ -1,9 +1,12 @@
 package co.id.adira.moservice.contentservice.repository;
 
 import java.util.List;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import co.id.adira.moservice.contentservice.model.PilihanLain;
 
 public interface PilihanLainRepository extends CrudRepository<PilihanLain, Long> {
+	
+	@Cacheable(cacheNames = "PilihanLainCache", key = "#root.targetClass.simpleName+'.'+#root.methodName")
 	List<PilihanLain> findTop4ByActiveOrderByIdAsc(boolean active);
 }
