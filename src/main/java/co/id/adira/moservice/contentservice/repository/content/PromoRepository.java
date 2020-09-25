1a0be14f425064014bb88868cfd24d53336c7a7a
Basuki Prabowo <basuki.mahardi@adira.co.id>
2020-09-25 10:15:44 +0700

    ix banner promo query #2 MOS-1388

--- src/main/java/co/id/adira/moservice/contentservice/repository/content/PromoRepository.java
+++ src/main/java/co/id/adira/moservice/contentservice/repository/content/PromoRepository.java
@@ -12,11 +12,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PromoRepository extends JpaRepository<Promo, Long> {
-	@Query(value = "SELECT *, GROUP_CONCAT(e.city_name) as cities, null AS bengkelNames " + "FROM mst_promo p "
+	@Query(value = "SELECT *, GROUP_CONCAT(DISTINCT e.city_name) as cities, null AS bengkelNames " + "FROM mst_promo p "
 			+ "JOIN db_content.map_promo_area d ON p.id = d.promo_id "
 			+ "JOIN db_bengkel.ref_city e ON d.city_id = e.city_id " + "WHERE p.zone_id = :zoneId AND p.is_active = true AND p.is_deleted = false "
 			+ "AND p.available_until > :currentDate " + "AND p.available_from < :currentDate "
-			+ "ORDER BY :#{#pageable}", nativeQuery = true)
+			+ "GROUP BY p.id ORDER BY :#{#pageable}", nativeQuery = true)
 	List<Promo> findAllByZoneIdAndMore(@Param("zoneId") Long zoneId, @Param("currentDate") Date currentDate,
 			@Param("pageable") Pageable pageable);
 
@@ -35,7 +35,7 @@ public interface PromoRepository extends JpaRepository<Promo, Long> {
 			@Param("promoTypeList") List<Integer> promoTypeList, @Param("currentDate") Date currentDate,
 			@Param("pageable") Pageable pageable);
 
-	@Query(value = "SELECT *, GROUP_CONCAT(e.city_name) as cities, null as bengkelNames FROM db_content.mst_promo a "
+	@Query(value = "SELECT *, GROUP_CONCAT(DISTINCT e.city_name) as cities, null as bengkelNames FROM db_content.mst_promo a "
 			+ "JOIN db_content.map_promo_service b on a.id = b.promo_id "
 			+ "JOIN db_servis.ref_tipe_servis c on b.service_umum_id = c.tipe_servis_id "
 			+ "JOIN db_content.map_promo_area d ON a.id = d.promo_id "
@@ -44,7 +44,7 @@ public interface PromoRepository extends JpaRepository<Promo, Long> {
 			+ "AND a.available_from < :currentDate GROUP BY a.id", nativeQuery = true)
 	Optional<Promo> findByIdAndMore(@Param("id") Long id, @Param("currentDate") Date currentDate);
 
-	@Query(value = "SELECT *, GROUP_CONCAT(e.city_name) as cities, null as bengkelNames  FROM db_content.mst_promo a "
+	@Query(value = "SELECT *, GROUP_CONCAT(DISTINCT e.city_name) as cities, null as bengkelNames  FROM db_content.mst_promo a "
 			+ "JOIN db_content.map_promo_service b on a.id = b.promo_id "
 			+ "JOIN db_servis.ref_tipe_servis c on b.service_umum_id = c.tipe_servis_id "
 			+ "JOIN db_content.map_promo_area d ON a.id = d.promo_id "
