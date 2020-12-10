dbd6486c60c5906a6ff02efc1db9b83f862e6e6a
fatchurrahman.fatchurrahman <fatchurrahman.fatchurrahman@adira.co.id>
2020-12-10 20:38:35 +0700

    fixing get promo

---
+++ src/main/java/co/id/adira/moservice/contentservice/repository/servis/ServiceTypeRepository.java
@@ -0,0 +1,8 @@
+package co.id.adira.moservice.contentservice.repository.servis;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import co.id.adira.moservice.contentservice.model.servis.ServiceType;
+
+public interface ServiceTypeRepository extends JpaRepository<ServiceType, Long>  {
+
+}
