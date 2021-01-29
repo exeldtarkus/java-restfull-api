7b262abc892dd773359c9013f18d4a5528b2c98d
Resthu Rizfarchan Adisurya <resthu.rizfarchan@adira.co.id>
2021-01-29 14:14:42 +0700

    group bengkel - fix query to exclude inactive and deleted group 2 - MOS-1964

--- src/main/java/co/id/adira/moservice/contentservice/repository/bengkel/GrupBengkelRepository.java
+++ src/main/java/co/id/adira/moservice/contentservice/repository/bengkel/GrupBengkelRepository.java
@@ -16,7 +16,7 @@ public interface GrupBengkelRepository extends JpaRepository <GrupBengkel, Long>
             + "LEFT JOIN map_group_bengkel b ON b.bengkel_id = a.bengkel_id " 
             + "LEFT JOIN mst_group_bengkel c ON c.id = b.group_bengkel_id " 
             + "WHERE a.bengkel_id IN (:bengkel_ids) "
-            + "AND c.is_active = 1 AND c.is_deleted = 0; "
+            + "AND (c.is_active = 1 OR c.is_active IS NULL) AND (c.is_deleted = 0 OR c.is_deleted IS NULL); "
             , nativeQuery = true)
     List<GrupBengkel> findAllByBengkelIds(@Param("bengkel_ids") List<Long> bengkel_ids);
 }
