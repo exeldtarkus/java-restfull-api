3a472ee1234b956cc9d8c2b21622f914bd9e4003
Resthu Rizfarchan Adisurya <resthu.rizfarchan@adira.co.id>
2021-02-02 13:58:07 +0700

    province & city - add province and city to list promo - MOS-2029

---
+++ src/main/java/co/id/adira/moservice/contentservice/dto/bengkel/ProvinceCityDTO.java
@@ -0,0 +1,16 @@
+package co.id.adira.moservice.contentservice.dto.bengkel;
+
+public interface ProvinceCityDTO{
+
+    Long getProvinceId();
+    
+    String getProvinceName();
+    
+    Long getCityId();
+
+    String getCityName();
+    
+}
+
+
+
