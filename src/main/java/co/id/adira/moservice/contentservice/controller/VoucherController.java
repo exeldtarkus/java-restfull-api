095b4174374ce19bce756c5097e6171874c95a6e
Resthu Rizfarchan Adisurya <resthu.rizfarchan@adira.co.id>
2021-04-28 14:36:52 +0700

    bugfix - add qrcode to emailEventDto and return value try 2 - MOS-2341

--- src/main/java/co/id/adira/moservice/contentservice/controller/VoucherController.java
+++ src/main/java/co/id/adira/moservice/contentservice/controller/VoucherController.java
@@ -93,7 +93,7 @@ public class VoucherController {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 		emailEventDto.setAvailableUntil(sdf.format(voucher.getPromo().getAvailableUntil()));
 		emailEventDto.setTo(authentication.getPrincipal().toString());
-		emailEventDto.setQRCodeBase64(qrcode.getBase64QRCode());
+		emailEventDto.setQRCodeBase64(qrcode.getQrcodePath());
 		
 		User user = new User();
 		user.setId(voucher.getUserId());
