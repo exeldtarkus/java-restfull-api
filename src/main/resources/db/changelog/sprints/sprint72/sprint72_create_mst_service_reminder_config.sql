1e95f4b479d792b8c1bf01b1ca453c566f5b7891
Jimmy Richard Timotius Dumalang <jimmy.dumalang@adira.co.id>
2022-02-04 00:33:29 +0700

    Create new table servis.mst_service_reminder_config - MOS-3908

---
+++ src/main/resources/db/changelog/sprints/sprint72/sprint72_create_mst_service_reminder_config.sql
@@ -0,0 +1,15 @@
+use servis;
+create table mst_service_reminder_config
+(
+    id             BIGINT auto_increment,
+    tipe_servis_id BIGINT                              null,
+    title          BIGINT                              null,
+    day            INT                                 null,
+    email_body     TEXT                                null,
+    created        timestamp default CURRENT_TIMESTAMP null,
+    updated        timestamp                           null on update CURRENT_TIMESTAMP,
+    constraint table_name_pk
+        primary key (id),
+    constraint mst_service_reminder_config_tipe_servis_id_fk
+        foreign key (tipe_servis_id) references ref_tipe_servis (tipe_servis_id)
+);
