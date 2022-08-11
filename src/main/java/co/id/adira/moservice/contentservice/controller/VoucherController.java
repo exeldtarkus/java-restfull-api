9d70fa5dee39b8925e9025238d7f817eec1902a9
Jimmy Richard Timotius Dumalang <jimmy.dumalang@adira.co.id>
2022-08-11 14:43:15 +0700

    MOS-4974 - set default empty array list

--- src/main/java/co/id/adira/moservice/contentservice/controller/VoucherController.java
+++ src/main/java/co/id/adira/moservice/contentservice/controller/VoucherController.java
@@ -1,6 +1,7 @@
 package co.id.adira.moservice.contentservice.controller;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -84,6 +85,7 @@ public class VoucherController {
 
 		List<String> utmIn = null;
 		if (utm != null) {
+			utmIn = new ArrayList<>();
 			if (utm.equals("adiraku-utm")) {
 				utmIn.add("adiraku");
 				utmIn.add("adirakupayment");
